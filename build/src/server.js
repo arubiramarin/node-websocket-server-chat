21304053f6981c1bba452a2e27f25937dec3034c
RUBIRA MARIN <XE04784@ad.bbva.com>
2018-02-19 12:08:15 +0100

    Enable CORS

--- build/src/server.js
+++ build/src/server.js
@@ -4,7 +4,11 @@ var WebSocket = require("ws");
 var models = require("../declarations/models");
 var port = 3000;
 var WebSocketServer = WebSocket.Server;
-var server = new WebSocketServer({ port: port });
+var server = new WebSocketServer({ port: port, headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "http://localhost:3000",
+        "Access-Control-Allow-Methods": "PUT, GET, POST, DELETE, OPTIONS"
+    } });
 server.on('connection', function (ws) {
     ws.on('message', function (message) {
         try {
--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,11 @@ import models = require('../declarations/models');
 
 var port: number = 3000;
 var WebSocketServer = WebSocket.Server;
-var server = new WebSocketServer({ port: port });
+var server = new WebSocketServer({ port: port, headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "http://localhost:3000",
+        "Access-Control-Allow-Methods": "PUT, GET, POST, DELETE, OPTIONS"
+} });
 
 server.on('connection', ws => {
 	ws.on('message', message => {
